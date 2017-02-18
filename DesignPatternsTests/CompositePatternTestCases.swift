da27cec06131aada96ca252b9673f0cff3af867c
jamesjbot <jongs.j@gmail.com>
2017-02-18 13:16:37 -0500

    Create test to make sure subnodes and leaves are added correctly to the structure
    Implement test case to add a sub node with three leaves.

--- DesignPatternsTests/CompositePatternTestCases.swift
+++ DesignPatternsTests/CompositePatternTestCases.swift
@@ -15,12 +15,13 @@ class CompositePatternTestCases: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        rootNode = Composite()
+        rootNode = Composite("Root")
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        rootNode = nil
     }
 
     func testCreation() {
@@ -29,15 +30,58 @@ class CompositePatternTestCases: XCTestCase {
 
     func testInsertionOfLeaf() {
         // When
-        rootNode?.add(component: Leaf())
-        let displacedLeaf = Leaf()
+        rootNode?.add(component: Leaf("Leaf 1"))
+        let displacedLeaf = Leaf("Leaf 2")
         displacedLeaf.move(distance: 3)
         rootNode?.add(component: displacedLeaf)
-        rootNode?.add(component: Leaf())
+        rootNode?.add(component: Leaf("Leaf 3"))
         // Given
         let movedLeaf = rootNode?.child(at: 1) as! Movable
         // Then
         XCTAssert(movedLeaf.position == 3, "The position is \(movedLeaf.position)")
     }
-    
+
+    func testRemoveOneLeaf() {
+        // When
+        rootNode?.add(component: Leaf("Leaf 1"))
+        let displacedLeaf = Leaf("Leaf 2")
+        displacedLeaf.move(distance: 3)
+        rootNode?.add(component: displacedLeaf)
+        rootNode?.add(component: Leaf("Leaf 3"))
+        // Given Remove middle node
+        rootNode?.remove(at: 1)
+        // Then
+        XCTAssert(rootNode?.children[0].name == "Leaf 1")
+        XCTAssert(rootNode?.children[1].name == "Leaf 3")
+    }
+
+    func testRemoveAll() {
+        // When
+        rootNode?.add(component: Leaf("Leaf 1"))
+        let displacedLeaf = Leaf("Leaf 2")
+        displacedLeaf.move(distance: 3)
+        rootNode?.add(component: displacedLeaf)
+        rootNode?.add(component: Leaf("Leaf 3"))
+        // Given Remove middle node
+        rootNode?.remove(at: 0)
+        rootNode?.remove(at: 0)
+        rootNode?.remove(at: 0)
+        // Then
+        XCTAssert(rootNode?.children.count == 0)
+    }
+
+    func testInsertNodeWithLeafs() {
+        // When
+        let subNode = Composite("SubNode 1")
+        subNode.add(component: Leaf("Leaf 1"))
+        subNode.add(component: Leaf("Leaf 2"))
+        subNode.add(component: Leaf("Leaf 3"))
+        // Given Remove middle node
+        rootNode?.add(component: subNode)
+        // Then
+        let result = (rootNode?.delineateChildren())
+        print(result!)
+        XCTAssert(result == "Root, SubNode 1, Leaf 1, Leaf 2, Leaf 3")
+    }
+
 }
